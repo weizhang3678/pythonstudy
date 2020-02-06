a7943db63016d3c35df9843dde005165c2c49b35
weizhang <wei.zhang3678@gmail.com>
2020-02-06 18:37:18 -0500

    turtle demoes

---
+++ turtle/rectangule.py
@@ -0,0 +1,31 @@
+'''
+Created on Feb 6, 2020
+
+@author: zhangwei
+'''
+# 导入turtle包的所有内容:
+from turtle import *
+
+# width of brush:
+width(4)
+
+# forward
+forward(200)
+# turn right
+right(90)
+
+# color of brush
+pencolor('red')
+forward(100)
+right(90)
+
+pencolor('green')
+forward(200)
+right(90)
+
+pencolor('blue')
+forward(100)
+right(90)
+
+# suspend this window, otherwise, it will be closed.
+done()
---
+++ turtle/star.py
@@ -0,0 +1,21 @@
+'''
+Created on Feb 6, 2020
+
+@author: zhangwei
+'''
+from turtle import *
+
+def drawStar(x, y):
+    pu()
+    goto(x, y)
+    pd()
+    # set heading: 0
+    seth(0)
+    for i in range(5):
+        fd(40)
+        rt(144)
+
+for x in range(0, 250, 50):
+    drawStar(x, 0)
+
+done()
