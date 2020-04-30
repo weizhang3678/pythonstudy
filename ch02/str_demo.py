49a4894d65564891a734250d9e7f45e176f537d3
weizhang <wei.zhang3678@gmail.com>
2020-04-30 10:23:52 -0400

    update str demo

---
+++ ch02/str_demo.py
@@ -0,0 +1,37 @@
+string1 = "I am a student."
+string2 = "Iam"
+string3 = "1.014"
+#slice
+print(string1[0])
+print(string1[:4])
+print(string1[-1])
+print(string1[0:8:2])
+print(string1[::-1])
+
+#function
+print(string1.capitalize())
+print(string1.casefold())
+print(string1.center(20))
+print(string1.center(20,'%'))
+print(string1.count('a'))
+print(string1.endswith('t.'))
+print(string1.startswith('i'))
+
+print(string1.find('a'))
+print(string1.index('a'))
+
+print(string1.upper())
+print(string1.lower())
+
+print(string1.istitle())
+print(string1.isupper())
+print(string1.islower())
+print(string1.strip())
+print(string1.lstrip())
+print(string1.rstrip())
+print(string1.swapcase())
+
+print(string3.isalpha())
+print(string3.isnumeric())
+print(string3.isdecimal())
+print(string3.isdigit())
